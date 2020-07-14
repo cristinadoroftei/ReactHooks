ee4676dc10d58f2e847587acf172e83bd4dd9917
Cristina <dorofteicristina19@gmail.com>
2020-07-14 18:08:01 +0200

    using useCallback and working with useRef for search input

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import "./Search.css";
 
-const Search = React.memo(props => {
+const Search = React.memo((props) => {
+  const { onLoadIngredients } = props;
+  const [enteredFilter, setEnteredFilter] = useState("");
+  const inputRef = useRef();
+
+  //used like this, useEffect() acts like componentDidUpdate: it runs the function after every component update
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current.value){
+        const query =
+        enteredFilter.length === 0
+          ? ""
+          : `?orderBy="title"&equalTo="${enteredFilter}"`;
+    fetch(
+      "https://react-hooks-update-ede58.firebaseio.com/ingredients.json" +
+        query
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        let loadedIngredients = [];
+        //the key is each unique id in ingredients
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        onLoadIngredients(loadedIngredients);
+      });
+      }
+
+    }, 500);
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [enteredFilter, onLoadIngredients]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            ref={inputRef}
+            type="text"
+            value={enteredFilter}
+            onChange={(event) => setEnteredFilter(event.target.value)}
+          />
         </div>
       </Card>
     </section>
